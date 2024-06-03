076af7f7d5dd059516780e70e351d3cf5208dcbf
oHaruo <89721034+oHaruo@users.noreply.github.com>
2024-06-03 16:58:08 +0900

    게임 매니저 완성
    게임 매니저 완성

--- Uni-Run/Assets/Scripts/ScrollingObject.cs
+++ Uni-Run/Assets/Scripts/ScrollingObject.cs
@@ -4,8 +4,12 @@
 public class ScrollingObject : MonoBehaviour {
     public float speed = 10f; // 이동 속도
 
-    private void Update() {
+    private void Update() 
+    {
         // 게임 오브젝트를 왼쪽으로 일정 속도로 평행 이동하는 처리
-        transform.Translate(Vector3.left*speed*Time.deltaTime);
+        if (!GameManager.instance.isGameover)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
     }
 }
