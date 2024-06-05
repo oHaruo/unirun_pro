cfb62cc3a5ff3544eb8c9db82bebb5d97eac0529
oHaruo <89721034+oHaruo@users.noreply.github.com>
2024-06-05 11:33:08 +0900

    유니런 완성
    유니런 완성
    플래포머 게임 제작 시작

--- Uni-Run/Assets/Scripts/Platform.cs
+++ Uni-Run/Assets/Scripts/Platform.cs
@@ -6,11 +6,31 @@ public class Platform : MonoBehaviour {
     private bool stepped = false; // 플레이어 캐릭터가 밟았었는가
 
     // 컴포넌트가 활성화될때 마다 매번 실행되는 메서드
-    private void OnEnable() {
+    private void OnEnable() 
+    {
         // 발판을 리셋하는 처리
+        stepped = false;
+
+        for(int i = 0; i<obstacles.Length; i++)
+        {
+            if(Random.Range(0,3) == 0 )
+            {
+                obstacles[i].SetActive(true);
+            }
+            else
+            {
+                obstacles[i].SetActive(false);
+            }
+        }
     }
 
-    void OnCollisionEnter2D(Collision2D collision) {
+    void OnCollisionEnter2D(Collision2D collision) 
+    {
         // 플레이어 캐릭터가 자신을 밟았을때 점수를 추가하는 처리
+        if(collision.collider.tag == "Player" && !stepped)
+        {
+            stepped = true;
+            GameManager.instance.AddScore(1);
+        }
     }
 }
